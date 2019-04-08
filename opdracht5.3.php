6f34a42e61e307c15b880857b5f24e0a1128d037
Taran Singh <taran.jhattu@gmail.com>
2019-04-08 15:02:28 +0200

    -Taran

--- opdracht5.3.php
+++ opdracht5.3.php
@@ -1,9 +1,9 @@
 <?php
 function idOptions()
 {
-    $dbc = mysqli_connect('localhost', 'root', '', 'dbchallenge') or die('Error connecting.');
+    $conn = mysqli_connect('localhost', 'root', '', 'dbchallenge') or die('Error connecting.');
     $query = "SELECT user_id FROM users";
-    $result = mysqli_query($dbc, $query);
+    $result = mysqli_query($conn, $query);
     $data = mysqli_fetch_all($result);
     echo "<option value=''></option>";
     foreach ($data as $values) {
@@ -11,5 +11,5 @@ function idOptions()
             echo "<option value='" . $value . "'>" . $value . "</option>";
         }
     }
-    mysqli_close($dbc);
+    mysqli_close($conn);
 }
